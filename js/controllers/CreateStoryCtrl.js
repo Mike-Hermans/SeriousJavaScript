23f931a90600c7cbe795d441acd74712463d708a
Mike-Hermans <mail@mikehermans.nl>
2015-06-21 13:51:53 +0200

    Added create stories

--- js/controllers/CreateStoryCtrl.js
+++ js/controllers/CreateStoryCtrl.js
@@ -5,7 +5,7 @@ app.controller('CreateStoryCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.create = function(story) {
         var request = $http({
             method: "POST",
-            url: "http://tools.mikehermans.nl/story_api/post_story.php",
+            url: "http://tools.mikehermans.nl/story_api/poststory.php",
             data: {
                 title: story.title,
                 description: story.description,
